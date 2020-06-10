e982aa0baf73624db7a37ce7e072c4b808a58c8c
Qsystem <devibong@qsystem-online.com>
2020-06-10 17:33:06 +0700

    devibong 2020-06-10 17:33

---
+++ application/controllers/Return_detail.php
@@ -0,0 +1,70 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Return_detail extends CI_Controller {
+    public function __construct(){
+        parent:: __construct();
+        $this->load->library('PHP_AES_Cipher');
+    }
+
+    public function index(){
+        $this->load->model("appid_model");
+
+        $key = "com_armex_key";
+
+        
+        //APPID|fstReturnId|CustCode
+        $token = $this->input->get("token");
+        $token = PHP_AES_Cipher::encrypt($key,"1234567890123456","JPU1|20200522201536121|_SSANT007");
+        $strResult = PHP_AES_Cipher::decrypt($key,$token);
+
+        $arrResult = explode("|",$strResult);
+        if (sizeof($arrResult) == 3){
+            $appId = $arrResult[0];
+            $returnId = $arrResult[1];            
+            $fstCustCode = $arrResult[2];            
+            $rwSales = $this->appid_model->getSales($appId,null,$fstCustCode);
+            if ($rwSales != null){
+                $returnId .= "_" .$rwSales->fst_sales_code;
+            }		
+
+
+            $ssql ="SELECT a.*,b.fst_sales_name,c.fst_cust_name FROM tr_return a 
+                inner join  tbsales b on a.fst_sales_code = b.fst_sales_code
+                inner join  tbcustomers c on a.fst_cust_code = c.fst_cust_code
+                where fst_return_id = ?";
+
+            $qr = $this->db->query($ssql,[$returnId]);
+            
+            $header = $qr->row_array();
+            //var_dump($header);
+            $data=[];
+            if ($header != null){
+                //echo $returnId;
+                $ssql = "SELECT a.*,b.fst_item_name from tr_return_details a 
+                    INNER JOIN tbitems b on a.fst_item_code = b.fst_item_code
+                    WHERE fst_return_id = ?";
+
+                $qr = $this->db->query($ssql,[$returnId]);
+
+                $details = $qr->result();
+                //var_dump($details);
+
+                $data["header"]=$header;
+                $data["details"]=$details;
+
+                $this->parser->parse('pages/return/receipt',$data);
+            }
+            
+
+        }else{
+            //Data Not ready;
+        }
+
+
+        //var_dump($token);
+        //var_dump($arrResult);
+    }
+
+
+}
---
+++ application/views/pages/return/receipt.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+	<head>		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Return Order</title>
+		<!-- Tell the browser to be responsive to screen width -->
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		<!-- Bootstrap 3.3.7 -->
+		<link rel="stylesheet" href="<?=base_url()?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="<?=base_url()?>bower_components/font-awesome/css/font-awesome.min.css">
+		<!-- bootstrap wysihtml5 - text editor -->
+		<link rel="stylesheet" href="<?=base_url()?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+		
+		<!-- jQuery 3 -->
+		<script src="<?=base_url()?>bower_components/jquery/dist/jquery.min.js"></script>				
+		<!-- CONFIG JS -->
+		<script src="<?=base_url()?>assets/system/js/config.js"></script>
+		<!-- APP JS -->
+		<script src="<?=base_url()?>assets/system/js/app.js"></script>
+		<style>
+			.block{
+				display:inline-block;
+			}
+
+		</style>
+	</head>
+	<body>
+		<div class="container-fluid" style="max-width:1024px"> 			
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<h3><b>BUKTI TANDA TERIMA BARANG RETUR</b></h3>
+				</div>
+			</div>
+			<div class="row" style="margin-top:20px">
+				<div class="col-xs-6">
+					<label class="block" style="width:100px;">Pelanggan</label>
+					<label class="block" style="width:10px;">:</label>                    
+					<span><?=$header["fst_cust_name"]?></span>
+				</div>
+				<div class="col-xs-6 text-right">
+					<label class="block text-left" style="width:100px;">No. Booking</label>
+					<label class="block" style="width:10px;">:</label>                    
+					<span class="block" style="width:200px;"><?=$header["fst_return_id"]?></span>
+				</div>				
+			</div>
+			<div class="row">
+				<div class="col-xs-6">
+					<label class="block" style="width:100px;">Sales</label>
+					<label class="block" style="width:10px;">:</label>                    
+					<span><?=$header["fst_sales_name"]?></span>
+				</div>
+				<div class="col-xs-6 text-right" >
+					<label class="block text-left" style="width:100px;">Tanggal</label>
+					<label class="block" style="width:10px;">:</label>                    
+					<span class="block" style="width:200px;"><?= date("d-M-Y",strtotime($header["fdt_return_datetime"]))?></span>
+				</div>				
+			</div>
+			
+			<div class="row" style="margin-top:20px">
+				<div class="col-xs-12">
+					<table class="table table-bordered table-condensed" style="width:100%">
+						<thead>
+							<tr>
+								<th class="text-center active">QTY</th>
+								<th class="text-center active">SATUAN</th>
+								<th class="text-center active">NAMA / SPESIFIKASI BARANG</th>
+								<th class="text-center active">KETERANGAN</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								foreach($details as $detail){
+									echo "<tr>
+										<td class='text-center'>$detail->fin_qty</td>
+										<td class='text-center'>$detail->fst_satuan</td>
+										<td class=''>$detail->fst_item_name</td>
+										<td class=''>$detail->fst_item_notes</td>										
+									</tr>";
+								}
+							?>							
+						</tbody>
+						<tfoot>
+							<tr>
+								<td colspan=4><?=$header["fst_notes"]?></td>
+							</tr>
+						</tfoot>
+					</table>
+					
+				</div>				
+			</div>
+		</div>
+		<!-- Bootstrap 3.3.7 -->
+		<script src="<?=base_url()?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+		<!-- Bootstrap WYSIHTML5 -->
+		<script src="<?=base_url()?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>		
+		<script src="<?=base_url()?>dist/js/app.js"></script>	
+	</body>
+</html>
